58b817d49f75076ad6a3f1b706b914454f745e28
Tam Pham <tam001p@gmail.com>
2025-09-17 17:07:08 -0500

    Refactor peer program into modular parts

--- peer/peer.py
+++ peer/peer.py
@@ -10,99 +10,117 @@ NAME = input('What\'s your name? ')
 # ID = f'{NAME}-{int(time.time())}'
 ID = f'{NAME}' # TODO: make unique
 
-peers = {}
-
 def decode_msg(data):
     return json.loads(data.decode())
 
 def encode_msg(msg) -> bytes:
     return json.dumps(msg).encode()
 
-def scan_for_peers():
+class Me:
     '''
-    Listens for peer announcements.
+    Handles a connection to the local network.
     '''
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', BROADCAST_PORT))
-    while True:
-        data, addr = sock.recvfrom(1024)
-        msg = decode_msg(data)
-        if msg['id'] == ID:
-            continue
-        if msg['type'] == 'announce':
-            peers[msg['id']] = {
-                'name': msg['name'],
-                'addr': addr,
-                'port': msg['port'],
-                'last_seen': time.time(),
-            }
+    def __init__(self, name: str):
+        self.name = name
 
-def scan_for_dms():
-    '''
-    Listens for direct messages from peers.
-    '''
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('', CHAT_PORT))
-    while True:
-        data, addr = sock.recvfrom(1024)
-        msg = decode_msg(data)
-        if msg['type'] == 'msg':
-            print(f'{peers.get(msg['from'], {'name': 'Unknown'})['name']} -> {NAME}: {msg['text']}')
+        self.known_peers = {}
 
-def announcer():
-    '''
-    Periodically announces presence to other peers over the local network.
-    '''
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    msg = encode_msg({
-        'id': ID,
-        'type': 'announce',
-        'name': NAME,
-        'port': CHAT_PORT,
-    })
-    while True:
-        sock.sendto(msg, ('<broadcast>', BROADCAST_PORT))
-        time.sleep(2)
+        self.scan_for_peers_thread = threading.Thread(target=self.scan_for_peers, daemon=True)
+        self.scan_for_dms_thread = threading.Thread(target=self.scan_for_dms, daemon=True)
+        self.announcer_thread = threading.Thread(target=self.announcer, daemon=True)
 
-def send_message(id, text):
-    if id not in peers:
-        print('Unknown peer ID')
-        return
-    peer = peers[id]
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    msg = encode_msg({
-        'type': 'msg',
-        'from': ID,
-        'text': text,
-    })
-    sock.sendto(msg, (peer['addr'][0], peer['port']))
-    print(f'{NAME} -> {peer['name']}: {text}')
-    # sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    # msg = json.dumps({'type': 'msg', 'from': NAME, 'text': text}).encode()
-    # # sock.sendto(msg, peer['addr'])
-    # sock.sendto(msg, ('<broadcast>', BROADCAST_PORT))
-    # print(f'To {peer['name']}: {text}')
+    def scan_for_peers(self):
+        '''
+        Listens for peer announcements and updates the known peers list. Should
+        be run in a separate thread.
+        '''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', BROADCAST_PORT))
+        while True:
+            data, addr = sock.recvfrom(1024)
+            msg = decode_msg(data)
+            if msg['id'] == ID:
+                continue
+            if msg['type'] == 'announce':
+                self.known_peers[msg['id']] = {
+                    'name': msg['name'],
+                    'addr': addr,
+                    'port': msg['port'],
+                    'last_seen': time.time(),
+                }
 
-# start threads
-threading.Thread(target=scan_for_peers, daemon=True).start()
-threading.Thread(target=scan_for_dms, daemon=True).start()
-threading.Thread(target=announcer, daemon=True).start()
+    def scan_for_dms(self):
+        '''
+        Listens for direct messages from peers and prints them to the console.
+        '''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('', CHAT_PORT))
+        while True:
+            data, addr = sock.recvfrom(1024)
+            msg = decode_msg(data)
+            if msg['type'] == 'msg':
+                print(f'{self.known_peers.get(msg['from'], {'name': 'Unknown'})['name']} -> {self.name}: {msg['text']}')
 
-print('''Commands:
+    def announcer(self):
+        '''
+        Periodically announces presence to other peers over the local network.
+        '''
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        msg = encode_msg({
+            'id': ID,
+            'type': 'announce',
+            'name': self.name,
+            'port': CHAT_PORT,
+        })
+        while True:
+            sock.sendto(msg, ('<broadcast>', BROADCAST_PORT))
+            time.sleep(2)
+
+    def send_message(self, id, text):
+        '''
+        Sends a direct message to a known peer. Prints an error if the peer
+        ID is not known.
+        '''
+        if id not in self.known_peers:
+            print('Unknown peer ID')
+            return
+        peer = self.known_peers[id]
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        msg = encode_msg({
+            'type': 'msg',
+            'from': ID,
+            'text': text,
+        })
+        sock.sendto(msg, (peer['addr'][0], peer['port']))
+        print(f'{self.name} -> {peer['name']}: {text}')
+
+    def start(self):
+        '''
+        Starts the connection threads and enters the command loop.
+        '''
+        self.scan_for_peers_thread.start()
+        self.scan_for_dms_thread.start()
+        self.announcer_thread.start()
+
+        print('''Commands:
 ls                 = list known peers
 msg <id> <message> = send message to peer''')
 
-while True:
-    print()
-    cmd = input('''> ''')
-    parts = cmd.split(' ', 2)
-    if parts[0] == 'ls':
-        for id, info in peers.items():
-            print(f'{info['name']} ({id}) last seen {time.time() - info['last_seen']:.1f}s ago')
-    elif parts[0] == 'msg' and len(parts) == 3:
-        send_message(parts[1], parts[2])
-    else:
-        print('Unknown command')
+        while True:
+            print()
+            cmd = input('''> ''')
+            parts = cmd.split(' ', 2)
+            if parts[0] == 'ls':
+                for id, info in self.known_peers.items():
+                    print(f'{info['name']} ({id}) last seen {time.time() - info['last_seen']:.1f}s ago')
+            elif parts[0] == 'msg' and len(parts) == 3:
+                self.send_message(parts[1], parts[2])
+            else:
+                print('Unknown command')
+
+if __name__ == '__main__':
+    me = Me(NAME)
+    me.start()
